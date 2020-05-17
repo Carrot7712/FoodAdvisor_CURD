97a993eef3da6c70b6d3c120397a9735b0dcdf17
Carrot7712 <carrot7712@gmail.com>
2020-05-17 21:09:55 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant')
+const restaurantList = require('../../restaurant.json')
+
+mongoose.connect('mongodb://localhost/restaurant-list', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  //新增json檔內的資料
+  restaurantList.results.forEach((restaurant)=>{
+    Restaurant.create({
+      name: restaurant.name,
+      name_en: restaurant.name_en,
+      category: restaurant.category,
+      image: restaurant.image,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      google_map: restaurant.google_map,
+      rating: restaurant.rating,
+      description: restaurant.description
+    })
+  })
+
+  console.log('done!')
+})
+
